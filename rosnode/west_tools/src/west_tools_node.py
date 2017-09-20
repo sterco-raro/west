1b544e9b15d07091887b9a12ad1bd6f8dd24baff
ciocias <imprenditoreciocci@gmail.com>
2017-09-20 14:28:33 +0200

    first implementation of new service

--- rosnode/west_tools/src/west_tools_node.py
+++ rosnode/west_tools/src/west_tools_node.py
@@ -5,6 +5,17 @@ import rospy
 from west_tools.srv import *
 from subprocess import Popen, PIPE
 
+wnodes = []
+
+class WNode():
+	"""docstring for WNode"""
+	def __init__(self, name, process):
+		super(WNode, self).__init__()
+		self.name = name
+		self.process = process
+
+		
+
 def handle_pack_list (req):
 	# spawn rospack process to retrive all packages in ros and put the result in a list
 	cmd = ['rospack', 'list-names']
@@ -56,16 +67,49 @@ def handle_service_list (req):
 def handle_run_node (req):
 	# use 'rosrun' command with package and nodes given in service request
 	cmd = ['rosrun', req.pack, req.node]
-	Popen (cmd, stdout = PIPE, stderr = PIPE)
+	process = Popen (cmd, stdout = PIPE, stderr = PIPE)
+	
+	# if is present a node with the same name we delete it
+	for e in wnodes:
+		if (e.name == req.node)
+			wnodes.remove(e)
 
-	return 1
+	# create new WNode and add it to list
+	node = WNode(req.node, process)
+	wnodes.append(node)
+
+	return node.poll() == None
 
 def handle_kill_node (req):
 	# use 'rosnode kill' command to kill requested node, ros takes care to do this dirty job
 	cmd = ['rosnode', 'kill', req.node]
-	Popen (cmd, stdout = PIPE, stderr = PIPE)
+	kill = Popen (cmd, stdout = PIPE, stderr = PIPE)
+	# wait util kill process exspire
+	kill.wait()
 
-	return 1
+	# check if node's process is normally die, else kill it
+	for e in wnodes:
+		if (e.name == req.node)
+			if (e.process.poll() == None)
+				e.process.kill()
+		# finally we remove this WNode from list
+		wnodes.remove()
+		return True
+
+	return False
+
+def handle_wnode_list (req):
+	return WNodeListResponse(wnodes)
+
+def handle_wnode_input (req):
+	# check if node requested is present, and if it is write given text to its standard input
+	for e in wnodes:
+		if (e.name == req.node)
+			if (e.process.poll() == None)
+				e.stdinput.write(req.text)
+				return True
+
+	return False
 
 if __name__ == '__main__':
 	
@@ -78,6 +122,16 @@ if __name__ == '__main__':
 	rospy.Service('service_list', ServiceList, handle_service_list)
 	rospy.Service('run_node', RunNode, handle_run_node)
 	rospy.Service('kill_node', KillNode, handle_kill_node)
+	rospy.Service('wnode_list', WNodeList, handle_wnode_list)
+	rospy.Service('wnode_input', WNodeInput, handle_wnode_input)
 
-	print 'Available services\n\t /pack_list\n\t /node_list <package>\n\t /service_list <node>\n\t /run_node <node>\n\t /kill_node <node>'
+	print '''Available services
+			\n\t /pack_list 
+			\n\t /node_list <package>
+			\n\t /service_list <node>
+			\n\t /run_node <node>
+			\n\t /kill_node <node>
+			\n\t /wnode_list
+			\n\t /wnode_input <node> <text>
+		'''
 	rospy.spin()
