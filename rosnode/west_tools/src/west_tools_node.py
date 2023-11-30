bb2d4aa57bb0fc8f9941244b7bbf6bf0a1d4e2a2
sterco-raro <unarmedpile@gmail.com>
2023-11-30 17:12:45 +0100

    Finished review for our ROS node, started testing. Some tests are easier using the UI so we'll do them while reviewing the UI code

--- rosnode/west_tools/src/west_tools_node.py
+++ rosnode/west_tools/src/west_tools_node.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # license removed for brevity
 
-import os
 import rospy
 from time import sleep
 from west_tools.srv import *
@@ -9,63 +8,92 @@ from subprocess import Popen, PIPE
 
 # ----- Globals ----------------------------------------------------------------
 
+# This node's name
+NODE_NAME = "west_tools_server"
+
 # Package list cache
 ROS_PKGS_CACHE = "/tmp/ros-pkgs-list.west"
 
 # Set of nodes launched by West
+# { node_name: { node_exec_name: <str>, package: <str>, process: <class Popen> } }
 wnodes = {}
 
 # ----- Utils ------------------------------------------------------------------
 
 def get_active_nodes ():
-	"""Query ROS and return a Set of currently active nodes (key: node name, value: ?)"""
-
+	"""Query ROS and return a Set of currently active nodes"""
 	# Get a list of active ROS nodes
 	cmd = [ "rosnode", "list" ]
-	nodes = launch_subprocess(cmd).split('\n')
+	nodes = launch_and_communicate_subprocess(cmd).split('\n')
 
-	# TODO Later on we'll do (newset - oldset) after launching a new node,
-	# FIXME I think we skip packages that spawn multiple nodes (later, inside handle_node_list)
+	# We use sets because they support arithmetic difference by simply doing (set1 - set2)
 	return set(nodes)
 
-def launch_subprocess (cmd):
+def launch_subprocess (cmd, stdin = None, stdout = PIPE, stderr = PIPE):
 	"""Launch a child process and return its (string) output"""
-	return Popen( cmd, stdout = PIPE, stderr = PIPE ).communicate()[0].strip()
+	# Popen executes a child program in a new process
+	return Popen( cmd, stdin = stdin, stdout = stdout, stderr = stderr )
+
+def launch_and_communicate_subprocess (cmd, stdin = None, stdout = PIPE, stderr = PIPE):
+	"""Launch a child process and return its (string) output"""
+	# Popen executes a child program in a new process
+	# .communicate() outputs a tuple with (stdout, stderr), avoiding possible buffer deadlocks
+	return Popen( cmd, stdin = stdin, stdout = stdout, stderr = stderr ).communicate()[0].strip()
 
 # ----- Services handlers ------------------------------------------------------
 
-def handle_pack_list (req):
-	"""Return a list of all ROS packages in the system"""
-	packages = None
+def handle_kill_node (req):
+	"""Terminate a node and and remove its instance from tracked nodes. Return False when something raises an exception"""
 	try:
-		with open(ROS_PKGS_CACHE, "r") as cache:
-			# Try to load cached list
-			packages = cache.read()
+		# Prepend backslash when missing
+		if req.node[0] != '/':
+			req.node = '/' + req.node
+
+		# Kill given node
+		cmd = [ "rosnode", "kill", req.node ]
+		process_kill = launch_subprocess(cmd)
+
+		# TODO necessary?
+		# Wait for process termination
+		process_kill.wait()
+		# Sleep a couple of seconds to guarantee subprocess "join"
+		sleep(2)
+
+		# Update wnodes when necessary
+		for key in wnodes:
+			if key == req.node or wnodes[key]["node_exec_name"] == req.node:
+				# Assert node termination
+				if wnodes[key]["process"].poll() == None:
+					# Kill node if it's still running
+					wnodes[key]["process"].kill()
+					# Refresh active nodes list
+					cmd = [ "rosnode", "cleanup" ]
+					launch_subprocess(cmd, stdin = PIPE).communicate("y")
+				# Delete dead node
+				del wnodes[key]
+
+	# Return False on error
 	except Exception as err:
-		with open(ROS_PKGS_CACHE, "w") as cache:
-			# Get packages list
-			cmd = [ "rospack", "list-names" ]
-			packages = launch_subprocess(cmd)
-			# Cache a copy of the list
-			cache.write(packages)
-	return PackListResponse(packages.split('\n'))
+		return KillNodeResponse(False)
+
+	return KillNodeResponse(True)
 
 def handle_node_list (req):
 	"""Return a list of all nodes available in the given package"""
-    # FIXME must be called multiple times when in web UI, why?
+    # TODO FIXME must be called multiple times when in web UI, why?
 	# Find package-related folders
 	cmd = [ "rospack", "find", req.pack ]
-	path_rospack = launch_subprocess(cmd).strip()
+	path_rospack = launch_and_communicate_subprocess(cmd)
 
 	cmd = [ "catkin_find", "--first-only", "--without-underlays", "--libexec", req.pack ]
-	path_catkin = launch_subprocess(cmd).strip()
+	path_catkin = launch_and_communicate_subprocess(cmd)
 
 	if path_rospack + path_catkin == "":
 		return NodeListResponse()
 
 	# Find node files in folders
 	cmd = [ "find", "-L", path_rospack, path_catkin, "-type", "f", "-perm", "/a+x", "-not", "-path", "'*/\.*'" ]
-	filepaths = launch_subprocess(cmd).strip().split('\n')
+	filepaths = launch_and_communicate_subprocess(cmd).split('\n')
 
 	if len(filepaths) == 0:
 		return NodeListResponse([])
@@ -82,15 +110,69 @@ def handle_node_list (req):
 
 	return NodeListResponse(nodes)
 
+def handle_pack_list (req):
+	"""Return a list of all ROS packages in the system"""
+	packages = None
+	try:
+		with open(ROS_PKGS_CACHE, "r") as cache:
+			# Try to load cached list
+			packages = cache.read()
+
+	except Exception as err:
+		with open(ROS_PKGS_CACHE, "w") as cache:
+			# Get packages list
+			cmd = [ "rospack", "list-names" ]
+			packages = launch_and_communicate_subprocess(cmd)
+			# Cache a copy of the list
+			cache.write(packages)
+
+	return PackListResponse(packages.split('\n'))
+
+def handle_run_node (req):
+	"""Launch a new node or try to replace an active node instance. Return True if the process is alive, False otherwise"""
+
+	# Currently active nodes
+	nodes_before = get_active_nodes()
+
+	# Launch requested node
+	cmd = [ "rosrun", req.pack, req.node ]
+	process = launch_subprocess(cmd, stdin = PIPE)
+
+	# Get the newly launched node(s)
+	diff_nodes = get_active_nodes() - nodes_before
+
+	# The diff is empty: we just replaced a running instance.
+	if len(diff_nodes) == 0:
+		# Try to find the instance inside wnodes
+		for key in wnodes:
+			# TODO possible BUG?
+			# TODO when a process replaces a running instance (e.g. turtlesim_node) we check the instance status via poll().
+			# TODO is it possible for poll to be called too early, thus preventing the process substitution and leaving us with an untracked process?
+			# TODO the new instance, the one we just launched, will be lost in the wind?
+			if wnodes[key]["package"] == req.pack and (key == req.node or wnodes[key]["node_exec_name"] == req.node):
+				# Check if the process has terminated
+				if wnodes[key]["process"].poll() != None:
+					# Replace terminated node with new process
+					wnodes[key]["process"] = process
+
+	# The diff is not empty: we launched a new node
+	else:
+		# NOT thread-safe. Other processes may have spawned new nodes in the meantime. These new nodes will end up having the wrong package and node_exec_name.
+		# Start tracking newly created nodes
+		for key in diff_nodes:
+			wnodes[key] = {}
+			wnodes[key]["node_exec_name"] = req.node
+			wnodes[key]["package"] = req.pack
+			wnodes[key]["process"] = process
+
+	return RunNodeResponse( process.poll() == None )
+
 def handle_service_list (req):
-	# TODO docstring
+	"""Return a list of available services"""
 
 	# Get services list for the given node
 	cmd = [ "rosnode", "info", req.node ]
-	output = launch_subprocess(cmd)
-
-	with open("/tmp/culo", "w") as fout:
-		fout.write(output)
+	output = launch_and_communicate_subprocess(cmd)
 
 	# Empty services list
 	if "Services: None" in output:
@@ -106,104 +188,51 @@ def handle_service_list (req):
 	services = filter( lambda name: True if "logger" not in name else False, services )
 	return ServiceListResponse(services)
 
-
-
-
-
-
-
-
-
-def handle_run_node (req):
-	# get a set of running nodes
-	nodes = get_active_nodes()
-
-	# use 'rosrun' command with package and nodes given in service request
-	cmd = ['rosrun', req.pack, req.node]
-	process = Popen (cmd, stdin = PIPE, stdout = PIPE, stderr = PIPE)
-	
-	# after new node launch, we require again a set of running nodes
-	# and make the difference (set difference) with this and the previous one
-	# to get the new entry, this is a new key for wnode
-	s = (get_active_nodes() - nodes)
-	key = None
-	if len(s) == 0 :
-		for k in wnodes :
-			if wnodes[k].poll() != None :
-				key = k
-	else :
-		key = s.pop()
-	# create entry on wnodes with 
-	# key : key 	node name
-	# value : process 	relative process 
-	if key != None :
-		wnodes[key] = process
-
-	# return true if process is alive, false otherwise
-	return RunNodeResponse(process.poll() == None)
-
-def handle_kill_node (req):
-	# we make sure the node name begins with the backslash
-	if req.node[0] != '/':
-		req.node = '/' + req.node
-	# use 'rosnode kill' command to kill node, ros takes care to do this dirty job
-	cmd = ['rosnode', 'kill', req.node]
-	kill = Popen (cmd, stdout = PIPE, stderr = PIPE)
-	# wait util kill process expire
-	kill.wait()
-	# sleep is necessary because wait doesn't guarantee subprocess join
-	sleep(2)
-	# check if node killed is a wnode, if it is remove this from wnodes
-	# check also if relative process still alive, in this case kill it too
-	if req.node in wnodes :
-		if wnodes[req.node].poll() == None:
-			wnodes[req.node].kill()
-			# refresh running node list
-			cmd = ['rosnode', 'cleanup']
-			Popen (cmd,stdin = PIPE, stdout = PIPE, stderr = PIPE).communicate('y')
-
-		del wnodes[req.node]
-
-	return KillNodeResponse(True)
-
-def handle_wnode_list (req):
-	# simply return a list of wnodes keys
-	return WNodeListResponse(wnodes.keys())
-
 def handle_wnode_input (req):
-	# we make sure the node name begins with the backslash
+	"""Send input to node via stdin"""
+	# Prepend backslash when missing
 	if req.node[0] != '/':
 		req.node = '/' + req.node
-	# check if node is present on wnodes and if it is still alive
-	# then is possible to write into its standard input
-	if req.node in wnodes :
-		if wnodes[req.node].poll() == None :
-			wnodes[req.node].stdin.write(req.text)
-			return WNodeInputResponse(True)
 
+	# Find target node, if it's stored inside wnode
+	for key in wnodes:
+		if key == req.node or wnodes[key]["node_exec_name"] == req.node:
+			# Check whether the target node is still alive
+			if wnodes[key]["process"].poll() == None:
+				# Send input
+				wnodes[key]["process"].communicate(req.text)
+				return WNodeInputResponse(True)
+	# No nodes available
 	return WNodeInputResponse(False)
 
-if __name__ == '__main__':
-	
-	print '\n--- Welcome to West tools server ---\n'
-	rospy.init_node('west_tools_server')
-	
-	# expose west services
-	rospy.Service('pack_list', PackList, handle_pack_list)
-	rospy.Service('node_list', NodeList, handle_node_list)
-	rospy.Service('service_list', ServiceList, handle_service_list)
-	rospy.Service('run_node', RunNode, handle_run_node)
-	rospy.Service('kill_node', KillNode, handle_kill_node)
-	rospy.Service('wnode_list', WNodeList, handle_wnode_list)
-	rospy.Service('wnode_input', WNodeInput, handle_wnode_input)
+def handle_wnode_list (req):
+	"""Return a list of nodes spawned by west"""
+	# Update wnodes status
+	for key in wnodes:
+		if wnodes[key]["process"].poll() != None:
+			# Refresh nodes list, purging unreachable nodes
+			cmd = [ "rosnode", "cleanup" ]
+			launch_subprocess(cmd, stdin = PIPE).communicate("y")
+			# Delete dead node
+			del wnodes[key]
+	# Return wnodes list
+	return WNodeListResponse(wnodes.keys())
 
-	print '''Available services :
-		\t /pack_list 
-		\t /node_list <package>
-		\t /service_list <node>
-		\t /run_node <node>
-		\t /kill_node <node>
-		\t /wnode_list
-		\t /wnode_input <node> <text>
-		'''
+# ----- Main entry point  ------------------------------------------------------
+
+if __name__ == '__main__':
+
+	# Setup this node with a custom name
+	rospy.init_node(NODE_NAME)
+	
+	# Register available services and related callbacks
+	rospy.Service('kill_node', KillNode, handle_kill_node)
+	rospy.Service('node_list', NodeList, handle_node_list)
+	rospy.Service('pack_list', PackList, handle_pack_list)
+	rospy.Service('run_node', RunNode, handle_run_node)
+	rospy.Service('service_list', ServiceList, handle_service_list)
+	rospy.Service('wnode_input', WNodeInput, handle_wnode_input)
+	rospy.Service('wnode_list', WNodeList, handle_wnode_list)
+
+	# Spin simply sleeps until shutdown
 	rospy.spin()
